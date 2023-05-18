e16fca87ce4b0bf2bc330cc6e83f619c700f25e6
bsenst <benjamin.senst@gmail.com>
2023-05-18 12:49:56 +0000

    add scripts

---
+++ taxi/train.py
@@ -0,0 +1,27 @@
+import os
+import pickle
+
+import mlflow
+
+mlflow.autolog()
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+def run_train(data_path: str):
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+    return rmse
+    
+run_train("./output")
